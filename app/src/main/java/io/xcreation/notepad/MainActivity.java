b9ae62d2235eed1ee4959f56c7d15aaffa953ad8
jxmked <jamc015lol@gmail.com>
2022-10-06 13:39:43 +0800

    Slow initial

--- app/src/main/java/io/xcreation/notepad/MainActivity.java
+++ app/src/main/java/io/xcreation/notepad/MainActivity.java
@@ -15,92 +15,120 @@ import android.app.Activity;
 import android.content.pm.*;
 import android.content.*;
 import android.net.*;
+import android.os.*;
+import android.*;
 
 public class MainActivity extends Activity {
     EditText editText1;
     String fileName;
-    Context MyContext;
-    //private String path;
-    //private File main_file;
-
+    private static String DATA_FOLDER = ".xio";
+	private String ROOT;
+	
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         getWindow().setSoftInputMode(3);
-
-        this.MyContext = this.getApplicationContext();
-
+		
+		// Check if external data folder does exists
+		
+		isStoragePermissionGranted();
         setContentView(R.layout.activity_main);
+		this.editText1 = (EditText) findViewById(R.id.mainTextField);
+		start();
+		
+	}
+	
+	protected void start(){
+		
+			try {
+				createExternalDataFolder();
+				toFile();
+				
+				File file = new File(this.fileName);
 
-        this.editText1 = (EditText) findViewById(R.id.mainTextField);
-        this.fileName = getResources().getString(R.string.file_name);
+				if(!file.exists()){
+					file.createNewFile();
+				}
 
+				FileInputStream fis = new FileInputStream(file);
+
+				byte[] bArr = new byte[fis.available()];
+
+				fis.read(bArr);
+
+				this.editText1.setText(new String(bArr));
+
+				fis.close();
+			} catch (Exception err) {
+				makeToast(err.toString());
+			}	
+		
+		
         
-		String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+ 	}
 
-	    String toFile = Paths.get("xio", "main.txt").toAbsolutePath().toString();
+	public void toFile(){
+		this.fileName = Paths.get(this.ROOT,getResources().getString(R.string.file_name)).toString();
+	}
+	
+	public void createExternalDataFolder() {
+		String __root__ = Environment.getExternalStorageDirectory().getAbsolutePath();
+		String path = Paths.get(MainActivity.DATA_FOLDER, getResources().getString(R.string.app_name)).toString();
+		File file = new File(__root__, path);
 		
+		if(! file.exists()) {
+		 	file.mkdirs();
+		}
 		
-		 // Check if directory does exists
-		File file = new File(path,toFile);
-		
-		//Toast.makeText(this.MyContext, folde, Toast.LENGTH_SHORT).show();
-		
-		if(! file.getParentFile().exists()) {
-		 	if(! file.mkdirs()){
-		 		Toast.makeText(this.MyContext, "Unable to create path", Toast.LENGTH_SHORT).show();
-		 		//finish();
-		 		//System.exit(-1);
+		this.ROOT = file.getAbsolutePath();
+		toFile();
+	}
+	
+	public  boolean isStoragePermissionGranted() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			if (
+				checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED &&
+				checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+			) {
+				requestPermissions(new String[]{
+					Manifest.permission.WRITE_EXTERNAL_STORAGE,
+					Manifest.permission.READ_EXTERNAL_STORAGE
+				}, 225);
 			}
 		}
 		
-		
-	 	//this.fileName = main_file.getAbsolutePath();
-		 
-        try {
-            FileInputStream openFileInput = openFileInput(this.fileName);
-
-            byte[] bArr = new byte[openFileInput.available()];
-
-            // Check if we can read it
-            openFileInput.read(bArr);
-
-            this.editText1.setText(new String(bArr));
-
-            openFileInput.close();
-        } catch (Exception err) {
-            Toast.makeText(this.MyContext, err.toString(), Toast.LENGTH_SHORT).show();
-        }
- 	}
+		//createExternalDataFolder();
+		return true;
+	}
 	
 	@Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 123: {
-					if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-						//If user presses allow
-						Toast.makeText(MainActivity.this, "Permission granted!", Toast.LENGTH_SHORT).show();
-						//Intent in = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + num.getText().toString()));
-						//startActivity(in);
-					} else {
-						//If user presses deny
-						Toast.makeText(MainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
-					}
-					break;
-				}
-        }
-    }
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+	{
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		
+		if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+		{
+			createExternalDataFolder();
+			start();
+		}else{
+			finish();
+		}
+	}
+	
     @Override
     public void onPause() {
         super.onPause();
         try {
-            FileOutputStream openFileOutput = openFileOutput(this.fileName, 0);
-            openFileOutput.write(this.editText1.getText().toString().getBytes());
-            openFileOutput.close();
-        } catch (Exception unused) {
-            Toast.makeText(this.MyContext, "Error while saving", Toast.LENGTH_SHORT).show();
+            
+            FileOutputStream fio = new FileOutputStream(new File(this.fileName));
+			fio.write(this.editText1.getText().toString().getBytes());
+            fio.close();
+        } catch (Exception err) {
+            makeToast(err.toString());
         }
     }
+	
+	public void makeToast(String str) {
+		Toast.makeText(this.getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+	}
 }
