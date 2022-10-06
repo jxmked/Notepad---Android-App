86d232dc66abfe65c954aaa7b19c9aca00a39a89
jxmked <jamc015lol@gmail.com>
2022-10-06 16:58:29 +0800

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Notepad---Android-App
-My first android notepad app
+# Note.
+
+***My first android notepad app***
+
+***Written using Android [AIDE](https://play.google.com/store/apps/details?id=com.aide.ui&hl=en&gl=US&referrer=utm_source%3Dgoogle%26utm_medium%3Dorganic%26utm_term%3Daide+apk&pcampaignid=APPU_1_FzQxY5DqIciK2roP3rSfwAw)***
+
+
+## Features:
+- Smooth scroll
+- Perfect dark text with white background
+- Like a single sheet of paper
+- Lightweight
+
+
+## TODO:
+- Add Dark and Light mode
+- Font size
+- Font text
+
+More...
+
+----
+
+***Written by Jovan De Guia***
+
+
+## Socials
+
+- [Github](https://github.com/jxmked)
+- [Facebook](https://www.facebook.com/deguia25)
---
+++ app/src/main/java/io/xcreation/notepad/MainActivity.java
@@ -0,0 +1,156 @@
+package io.xcreation.notepad;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Path;
+import android.app.Activity;
+import java.nio.file.Paths;
+import android.os.Build;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.widget.ScrollView;
+
+public class MainActivity extends Activity
+{
+    EditText editText1;
+	ScrollView scrollView;
+    String fileName;
+    private static String DATA_FOLDER_NAME = ".xio";
+	private static String DATA_FOLDER;
+
+    @Override
+    public void onCreate(Bundle bundle)
+	{
+        super.onCreate(bundle);
+        getWindow().setSoftInputMode(3);
+        setContentView(R.layout.activity_main);
+		this.editText1 = (EditText) findViewById(R.id.mainTextField);
+		this.scrollView = (ScrollView) findViewById(R.id.scrollView);
+
+		String __root__ = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+		// Overwrite External Data folder with absolute path
+		MainActivity.DATA_FOLDER = Paths.get(__root__, MainActivity.DATA_FOLDER_NAME, getResources().getString(R.string.app_name)).toString();
+
+		this.fileName = Paths.get(MainActivity.DATA_FOLDER, "main.txt").toString();
+
+		// Check if permission is already granted
+		// If not, request.
+		if (isStoragePermissionGranted())
+		{
+			start();
+		}
+
+		// TODO: Touch layout then focus on textfield
+	}
+
+	protected void start()
+	{
+		try
+		{	
+			File file = new File(this.fileName);
+
+			if (!file.exists())
+			{
+				file.createNewFile();
+			}
+
+			FileInputStream fis = new FileInputStream(file);
+
+			byte[] bArr = new byte[fis.available()];
+
+			fis.read(bArr);
+
+			this.editText1.setText(new String(bArr));
+
+			fis.close();
+		}
+		catch (Exception err)
+		{
+			makeToast(err.toString());
+		}
+ 	}
+
+	public void createFolder()
+	{
+		File file = new File(MainActivity.DATA_FOLDER);
+
+		if (! file.exists())
+		{
+		 	file.mkdirs();
+		}
+	}
+
+	public  boolean isStoragePermissionGranted()
+	{
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+		{
+			if (
+				checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED &&
+				checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
+				)
+			{
+				// Requests permission
+				requestPermissions(new String[]{
+									   Manifest.permission.WRITE_EXTERNAL_STORAGE,
+									   Manifest.permission.READ_EXTERNAL_STORAGE
+								   }, 225);
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+	{
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+		if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+		{
+			// Create Data Folder if not exists
+			createFolder();
+			start();
+		}
+		else
+		{
+			finish();
+		}
+	}
+
+	public void showSoftKeyboard()
+	{
+		// Focus on TextField
+		this.editText1.setFocusableInTouchMode(true);
+		this.editText1.requestFocus();
+	}
+
+    @Override
+    public void onPause()
+	{
+        super.onPause();
+        try
+		{
+
+            FileOutputStream fio = new FileOutputStream(new File(this.fileName));
+			fio.write(this.editText1.getText().toString().getBytes());
+            fio.close();
+        }
+		catch (Exception err)
+		{
+            makeToast(err.toString());
+        }
+    }
+
+	public void makeToast(String str)
+	{
+		Toast.makeText(this.getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+	}
+}
