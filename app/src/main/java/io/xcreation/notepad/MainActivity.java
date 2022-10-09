2d5af37d31ac100cddb41189d5d1b0dd5038ed60
Jovan <jamc015lol@gmail.com>
2022-10-09 16:23:54 +0800

    attempting to add dark/light mode toggle

--- app/src/main/java/io/xcreation/notepad/MainActivity.java
+++ app/src/main/java/io/xcreation/notepad/MainActivity.java
@@ -1,59 +1,49 @@
 package io.xcreation.notepad;
 
 import android.Manifest;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.ScrollView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editText1;
-    ScrollView scrollView;
-    String fileName;
-    final private static String DATA_FOLDER_NAME = ".xio";
+    private final EditText editText1 = (EditText) findViewById(R.id.mainTextField);
+    private final View darkMode_toggle = findViewById(R.id.toggle_dark_mode);
+    private final static String DATA_FOLDER_NAME = ".xio";
     private static String DATA_FOLDER;
+    private String fileName;
+    private final SharedPreferences Pref = getSharedPreferences("Setting", 0);
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         getWindow().setSoftInputMode(3);
 
-        int nightModeFlags = getResources().getConfiguration().uiMode &
-                Configuration.UI_MODE_NIGHT_MASK;
-        switch (nightModeFlags) {
-            case Configuration.UI_MODE_NIGHT_YES:
-                setContentView(R.layout.activity_main_dark);
-                break;
-
-            case Configuration.UI_MODE_NIGHT_NO:
-                setContentView(R.layout.activity_main);
-                break;
-
-            case Configuration.UI_MODE_NIGHT_UNDEFINED:
-                setContentView(R.layout.activity_main);
-                break;
-        }
-
-        this.editText1 = (EditText) findViewById(R.id.mainTextField);
-        this.scrollView = (ScrollView) findViewById(R.id.scrollView);
+        setContentView(R.layout.activity_main);
 
         final String __root__ = Environment.getExternalStorageDirectory().getAbsolutePath();
 
         // Overwrite External Data folder with absolute path
-        MainActivity.DATA_FOLDER = Paths.get(__root__, MainActivity.DATA_FOLDER_NAME, getResources().getString(R.string.app_name)).toString();
-
-        this.fileName = Paths.get(MainActivity.DATA_FOLDER, "main.txt").toString();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            MainActivity.DATA_FOLDER = Paths.get(__root__, MainActivity.DATA_FOLDER_NAME, getResources().getString(R.string.app_name)).toString();
+            this.fileName = Paths.get(MainActivity.DATA_FOLDER, String.valueOf(R.string.file_name)).toString();
+        } else {
+            MainActivity.DATA_FOLDER = __root__ + MainActivity.DATA_FOLDER_NAME + getResources().getString(R.string.app_name);
+            this.fileName = MainActivity.DATA_FOLDER + R.string.file_name;
+        }
 
         // Check if permission is already granted
         // If not, request.
@@ -62,18 +52,58 @@ public class MainActivity extends AppCompatActivity {
             start();
         }
 
-        // TODO: Touch layout/Scrollview then focus on textfield
-        /**
-        final ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
-        scrollView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                showSoftKeyboard(view);
-                return false;
-            }
-        });
-        **/
+        // Load Theme
+       /* String theme = toggle_dark_light_mode();
+        changeTheme(theme);
 
+        // TODO: Add event listener to toggle between dark and light mode
+        darkMode_toggle.setOnClickListener(v -> {
+
+            // Get current theme
+            String theme1 = toggle_dark_light_mode();
+
+            // Switch light to dark mode
+            if(theme1.equals("light")) {
+                set_pref_theme("dark");
+            } else {
+                set_pref_theme("light");
+            }
+
+            // Get latest changes then apply
+            theme1 = toggle_dark_light_mode();
+            changeTheme(theme1);
+        }); */
+
+    }
+
+    void set_pref_theme(String latest_theme){
+        SharedPreferences.Editor setting = this.Pref.edit();
+        setting.putString("theme", latest_theme);
+        setting.apply();
+    }
+
+    String toggle_dark_light_mode(){
+        // We don't apply a default value to tell us that it is not set
+        String theme = this.Pref.getString("theme", "");
+
+        if(theme.equals("")) {
+            // No preferences
+            SharedPreferences.Editor setting = this.Pref.edit();
+            setting.putString("theme", "light");
+            setting.apply();
+
+            theme = "light";
+        }
+
+       return theme;
+    }
+
+    protected void changeTheme(String mode) {
+        if(Objects.equals(mode, "dark")) {
+            setContentView(R.layout.activity_main_dark);
+        } else {
+            setContentView(R.layout.activity_main);
+        }
     }
 
     protected void start() {
@@ -125,7 +155,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -138,22 +168,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-    public void showSoftKeyboard(View view) {
-        // Focus on TextField
-        this.editText1.requestFocus();
-
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.showSoftInput(view, InputMethodManager.SHOW_FORCED);
-
-    }
-    **/
     @Override
     public void onPause() {
         super.onPause();
         try {
 
-            FileOutputStream fio = new FileOutputStream(new File(this.fileName));
+            FileOutputStream fio = new FileOutputStream(this.fileName);
             fio.write(this.editText1.getText().toString().getBytes());
             fio.close();
         } catch (Exception err) {
